ae80448ebf98e0047e638a248ac9e97908a21e21
Saurav Tiwari <116860218+saurav-tiwari03@users.noreply.github.com>
2024-08-24 22:41:25 +0530

    middlware

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,7 +5,7 @@ export function middleware(request: NextRequest) {
   const cookieValue = request.cookies.get('access')?.value;
   const currentUrl = new URL(request.url);
 
-  if (cookieValue === 'true' && currentUrl.pathname === '/admin') {
+  if (cookieValue === 'true' && currentUrl.pathname !== '/admin/dashboard') {
     return NextResponse.redirect(new URL('/admin/dashboard', request.url));
   } else if (cookieValue !== 'true' && currentUrl.pathname !== '/admin/login') {
     return NextResponse.redirect(new URL('/admin/login', request.url));
